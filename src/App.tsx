a5bb8aeaa2105e4fc90efa695741fb2b1cb2afcb
Michael Quarcoo <michaelquarcoo04@gmail.com>
2024-05-11 05:29:05 +0000

    Add icons using Icon component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import "./App.css";
+import { Icon } from "./components";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Icon name="bxl-typescript" size="36px" />
+      <Icon name="bx-plus" size="36px" />
+      <Icon name="bxl-react" size="36px" />
+      <h1>We go again</h1>
+      <Icon name="bx-rocket" size="36px" />
+      <Icon name="bx-rocket" size="36px" />
+    </div>
+  );
 }
 
 export default App;
